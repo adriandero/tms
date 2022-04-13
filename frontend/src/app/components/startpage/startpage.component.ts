042394f6d8b624c7b785516f1d912d230f9966c7
DeRo Adrian <Adrian.DeRo@snt.at>
2022-04-13 15:25:12 +0200

    updated filter

--- frontend/src/app/components/startpage/startpage.component.ts
+++ frontend/src/app/components/startpage/startpage.component.ts
@@ -1,48 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import {faFilter, faSort, faBars} from "@fortawesome/free-solid-svg-icons"
-import {MatDialog} from "@angular/material/dialog";
-import {TenderOverlayComponent} from "../tender-overlay/tender-overlay.component";
-import {FilterOverlayComponent} from "../filter-overlay/filter-overlay.component";
-
-
+import { faFilter, faSort, faBars } from '@fortawesome/free-solid-svg-icons';
+import { MatDialog } from '@angular/material/dialog';
+import { TenderOverlayComponent } from '../tender-overlay/tender-overlay.component';
+import { FilterOverlayComponent } from '../filter-overlay/filter-overlay.component';
 
 @Component({
   selector: 'app-startpage',
   templateUrl: './startpage.component.html',
-  styleUrls: ['./startpage.component.css']
+  styleUrls: ['./startpage.component.css'],
 })
 export class StartpageComponent implements OnInit {
-
-  constructor(public filterDialog: MatDialog, public sortDialog: MatDialog) {
-  }
+  constructor(public filterDialog: MatDialog, public sortDialog: MatDialog) {}
   public showfilter: boolean = true;
   public showsort: boolean = true;
 
-  public toogle(){
-    this.showfilter = !this.showfilter
+  public toogle() {
+    this.showfilter = !this.showfilter;
     console.log(this.showfilter);
   }
 
-  public toggleSort(){
-    this.showsort = !this.showsort
-
+  public toggleSort() {
+    this.showsort = !this.showsort;
   }
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   faFilter = faFilter;
   faBars = faBars;
   faSort = faSort;
-  sorts: string[] = ["alphapetical a-z", "alphapetical z-a", "By relevance", "Latest", "Oldest"];
-
-
+  sorts: string[] = [
+    'alphapetical a-z',
+    'alphapetical z-a',
+    'By relevance',
+    'Latest',
+    'Oldest',
+  ];
 
   openDialog() {
     const dialogRef = this.filterDialog.open(FilterOverlayComponent, {
       closeOnNavigation: true,
-      maxWidth: '500px',
-      minWidth: '350px',
-      maxHeight: '500px',
+      maxWidth: '550px',
+      minWidth: '400px',
+      maxHeight: '550px',
       minHeight: '200px',
       width: '80vw',
       height: 'auto',
