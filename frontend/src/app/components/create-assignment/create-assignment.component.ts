aa8e7ce32881ec30350b8d137153c92ffe043e94
GERBER Lilia <lilia.gerber@snt.at>
2022-06-12 21:25:10 +0200

    createa assignment component

--- frontend/src/app/components/create-assignment/create-assignment.component.ts
+++ frontend/src/app/components/create-assignment/create-assignment.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+import {Tender, User} from "../../model/Tender";
 
+export interface Assignment{
+  tender : Tender;
+  id : number;
+  user : string; //TODO user id or user
+  hasUnseenChanges : boolean;
+  instruction : string;
+
+}
 @Component({
   selector: 'create-assignment',
   templateUrl: 'create-assignment.component.html',
   styleUrls: ['./create-assignment.component.css'],
 })
+
+
 export class CreateAssignmentComponent {
+
+
+  assignments : Assignment[]= []
+  constructor( @Inject(MAT_DIALOG_DATA) public data: any,) {
+    this.assignments = data.assignments
+    console.log(this.assignments)
+  }
+
   userOptions: string[] = ['exampl@gmail.com'];
+  task :string = "";
+  user : string;
+
+  create(){
+    let ass : Assignment= {tender: this.data.tender, id: 1, instruction: this.task, hasUnseenChanges: true, user: this.user }
+    this.assignments.push(ass)
+  }
+
 }
--- frontend/src/app/components/tender-overlay/tender-overlay.component.ts
+++ frontend/src/app/components/tender-overlay/tender-overlay.component.ts
@@ -1,18 +1,18 @@
-import { Component, Inject, Injectable } from '@angular/core';
+import {Component, Inject, Injectable} from '@angular/core';
 import {
   faArrowLeft,
   faArrowRight,
   faFileAlt,
   faTimes,
 } from '@fortawesome/free-solid-svg-icons';
-import { Router } from '@angular/router';
-import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog'; // import router from angular router
-import { AssignedIntStatus, Tender } from 'src/app/model/Tender';
-import { TenderService } from '../../services/tender.service';
-import { User } from '../../model/User';
-import { Hidden } from '@material-ui/core';
+import {Router} from '@angular/router';
+import {MAT_DIALOG_DATA, MatDialog} from '@angular/material/dialog'; // import router from angular router
+import {AssignedIntStatus, Tender} from 'src/app/model/Tender';
+import {TenderService} from '../../services/tender.service';
+import {User} from '../../model/User';
+import {Hidden} from '@material-ui/core';
 
-import { CreateAssignmentComponent } from '../create-assignment/create-assignment.component';
+import {CreateAssignmentComponent} from '../create-assignment/create-assignment.component';
 
 // import CloseIcon from '@material-ui/icons/Close';
 @Component({
@@ -25,14 +25,16 @@ export class TenderOverlayComponent {
   faTimes = faTimes;
   faFileAlt = faFileAlt;
   faArrowLeft = faArrowLeft;
-
+  assignments: any = [];
   hasUpdates: boolean = true;
+
   // CloseIcon = CloseIcon
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: Tender,
     public dialog: MatDialog,
     private route: Router
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
     if (this.data.updates.length == 0) {
@@ -61,6 +63,9 @@ export class TenderOverlayComponent {
       closeOnNavigation: true,
       maxWidth: '400px',
       maxHeight: '300px',
+      data: {
+        dataKey: {assignments : this.assignments, tender : this.data}
+      }
     });
   }
 
@@ -77,6 +82,7 @@ export class TenderOverlayComponent {
   closeDialog() {
     this.dialog.closeAll();
   }
+
   // go(){
   // this.dialogRef.close();
   //this.route.navigate(['/history']);
